12774a18003bb26e51c3549816a5dd71e1fb459e
Nan Andrei David <andrei.nan03@e-uvt.ro>
2023-05-18 13:03:56 +0300

    base commit

---
+++ Source/Headers/Global.h
@@ -0,0 +1,17 @@
+//
+// Created by David on 5/18/2023.
+//
+#pragma once
+
+#ifndef PP_SPACE_INVASION_GLOBAL_H
+#define PP_SPACE_INVASION_GLOBAL_H
+
+#endif
+
+
+
+// required when drawing the window
+constexpr unsigned char SCREEN_RESIZE = 4;
+constexpr unsigned short SCREEN_HEIGHT = 180;
+constexpr unsigned short SCREEN_WIDTH = 320;
+dw
---
+++ Source/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <SFML/Graphics.hpp> // for display
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+
+#include <chrono>             // for timing
+#include <random>
+
+#include "Headers/Global.h"
+
+
+int main() {
+
+    std::chrono::microseconds lag(0);
+    std::chrono::steady_clock::time_point prev_time;
+
+    std::mt19937_64 random_engine(
+            std::chrono::system_clock::now().time_since_epoch().count()
+            );
+
+    sf::RenderWindow window(sf::VideoMode(SCREEN_RESIZE * SCREEN_WIDTH, SCREEN_RESIZE * SCREEN_HEIGHT),
+                            "TEST-title-idk",
+                            sf::Style::Close);
+
+    window.setView(sf::View(sf::FloatRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)));
+
+
+
+    window.display();
+
+    return 0;
+}
