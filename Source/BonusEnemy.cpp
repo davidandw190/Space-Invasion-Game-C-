e2a78db403c4ec0526ba08b197255fc9dbc67b21
Nan Andrei David <andrei.nan03@e-uvt.ro>
2023-06-12 18:36:35 +0300

    Worked on the source file for the bonus enemy;

--- Source/BonusEnemy.cpp
+++ Source/BonusEnemy.cpp
@@ -1,3 +1,34 @@
+////
+//// Created by David on 6/12/2023.
+////
+//#include "Headers/BonusEnemy.hpp"
+//#include "Headers/Global.hpp"
 //
-// Created by David on 6/12/2023.
+//BonusEnemy::BonusEnemy(std::mt19937_64 &random_engine):
+//        Entity(),
+//        dead_animation_over(false),
+//        explosion_x(0),
+//        timer_distrib(0)
+//
+//        {
+//            this->y = BASE_SIZE;
+//            this->powerup_distrib = std::uniform_int_distribution<unsigned short>(0, POWERUP_TYPES - 1);
+//            this->timer_distrib = std::uniform_int_distribution<unsigned short>(BONUS_TIMER_MIN, BONUS_TIMER_MAX);
+//            this->animation = AnimationManager(
+//                    BONUS_ANIMATION_SPEED, 2 * BASE_SIZE, R"(C:\Users\40787\Desktop\PP-SPACE-INVASION\Source\Resources\BonusEnemy.png)");
+//            this->explosion = AnimationManager(
+//                    EXPLOSION_ANIMATION_SPEED, 2 * BASE_SIZE, R"(C:\Users\40787\Desktop\PP-SPACE-INVASION\Source\Resources\ExplosionBig.png)");
+//
+//            reset(true, random_engine);
+////
+////            for (unsigned char a = 0; a < POWERUP_TYPES; a++) {
+////                powerup_animations.push_back(Animation(POWERUP_ANIMATION_SPEED, BASE_SIZE, "Resources/Images/Powerup" + std::to_string(static_cast<unsigned short>(a)) + ".png"));
+////            }
+//
+//            this->powerup_animations.push_back(AnimationManager(POWERUP_ANIMATION_SPEED, BASE_SIZE,
+//                                                         R"(C:\Users\40787\Desktop\PP-SPACE-INVASION\Source\Resources\BonusEnemy.png)"));
+//        }
+//
+//
+//
 //
--- Source/Headers/Global.hpp
+++ Source/Headers/Global.hpp
@@ -19,6 +19,8 @@ constexpr unsigned char BASE_SIZE = 16;
 constexpr unsigned char SCREEN_RESIZE = 4;
 constexpr unsigned short SCREEN_HEIGHT = 180;
 constexpr unsigned short SCREEN_WIDTH = 320;
+constexpr unsigned char NEXT_LEVEL_TRANSITION = 64;
+constexpr unsigned char TOTAL_LEVELS = 8;
 
 // player related stuff
 constexpr unsigned char PLAYER_MOVE_SPEED = 2;
@@ -27,6 +29,7 @@ constexpr unsigned char RELOAD_DURATION = 30;
 constexpr unsigned char FAST_RELOAD_DURATION = 6;
 
 // Enemy related stuff
+constexpr unsigned char ENEMY_TYPES = 3;
 constexpr unsigned char ENEMY_HIT_TIMER_DURATION = 2;
 constexpr unsigned char ENEMY_MOVE_PAUSE_MIN = 3;
 constexpr unsigned short ENEMY_SHOOT_CHANCE_INCREASE = 64;
@@ -38,11 +41,20 @@ constexpr unsigned char ENEMY_MOVE_PAUSE_START_MIN = 47;
 // the more enemies we kill, the faster they become.
 constexpr unsigned char ENEMY_MOVE_PAUSE_DECREASE = 1;
 constexpr unsigned char ENEMY_BULLET_SPEED = 2;
-constexpr unsigned short ENEMY_SHOOT_CHANCE = 2080;
+constexpr unsigned short ENEMY_SHOOT_CHANCE = 3000;
+
+// bonus enemy stuff
+constexpr unsigned char BONUS_ANIMATION_SPEED = 8;
+constexpr unsigned char BONUS_MOVE_SPEED = 1;
+constexpr unsigned short BONUS_TIMER_MAX = 1024;
+constexpr unsigned short BONUS_TIMER_MIN = 768;
 
 constexpr unsigned char EXPLOSION_ANIMATION_SPEED = 2;
 
 constexpr unsigned char POWERUP_DURATION = 512;
+constexpr unsigned char POWERUP_ANIMATION_SPEED = 16;
+constexpr unsigned char POWERUP_SPEED = 2;
+constexpr unsigned char POWERUP_TYPES = 4;
 
 // a constant for the frame duration in ms
 constexpr std::chrono::microseconds FRAME_DURATION(16667);
