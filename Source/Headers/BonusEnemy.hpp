32986d9a96cb9b479da1452efad679d970e227c3
Nan Andrei David <andrei.nan03@e-uvt.ro>
2023-06-14 13:00:13 +0300

    Finished source files for the bonus enemy;

--- Source/Headers/BonusEnemy.hpp
+++ Source/Headers/BonusEnemy.hpp
@@ -1,46 +1,49 @@
-////
-//// Created by David on 6/12/2023.
-////
 //
-//#include "Entity.hpp"
-//#include "AnimationManager.hpp"
-//#include "Powerup.hpp"
+// Created by David on 6/12/2023.
 //
-//#ifndef PP_SPACE_INVASION_BONUSENEMY_HPP
-//#define PP_SPACE_INVASION_BONUSENEMY_HPP
-//
-//class BonusEnemy: public Entity {
-//
-//protected:
-//    bool dead;
-//    bool dead_animation_over;
-//
-//    short explosion_x;
-//
-//    std::uniform_int_distribution<unsigned short> powerup_distrib;
-//
-//    std::uniform_int_distribution<unsigned short> timer_distrib;
-//
-//    std::vector<AnimationManager> powerup_animations;
-//
-//    std::vector<Powerup> powerups;
-//
-//    AnimationManager animation;
-//    AnimationManager explosion;
-//public:
-//    explicit BonusEnemy(std::mt19937_64& random_engine);
-//
-//    bool check_bullet_collision(std::mt19937_64& random_engine, const sf::IntRect& bullet_hitbox);
-//
-//    unsigned char check_powerup_collision(const sf::IntRect& player_hitbox);
-//
-//    void draw(sf::RenderWindow& window);
-//    void reset(bool dead, std::mt19937_64& random_engine);
-//    void update(std::mt19937_64& random_engine);
-//
-//    sf::IntRect get_hitbox() const;
-//
-//};
-//
-//
-//#endif //PP_SPACE_INVASION_BONUSENEMY_HPP
+
+#include "Entity.hpp"
+#include "AnimationManager.hpp"
+#include "Powerup.hpp"
+
+#ifndef PP_SPACE_INVASION_BONUSENEMY_HPP
+#define PP_SPACE_INVASION_BONUSENEMY_HPP
+
+class BonusEnemy: public Entity {
+
+protected:
+    bool dead;
+    bool dead_animation_over;
+
+    short explosion_x;
+
+    unsigned short timer;
+
+    std::uniform_int_distribution<unsigned short> powerup_distrib;
+
+    std::uniform_int_distribution<unsigned short> timer_distrib;
+
+    std::vector<AnimationManager> powerup_animations;
+
+    std::vector<Powerup> powerups;
+
+    AnimationManager animation;
+    AnimationManager explosion;
+public:
+    explicit BonusEnemy(std::mt19937_64& random_engine);
+
+    bool check_bullet_collision(std::mt19937_64& random_engine, const sf::IntRect& bullet_hitbox);
+
+    unsigned char check_powerup_collision(const sf::IntRect& player_hitbox);
+
+    void draw(sf::RenderWindow& window);
+    void reset(bool dead, std::mt19937_64& random_engine);
+    void update(std::mt19937_64& random_engine);
+
+    sf::IntRect get_hitbox() const;
+
+    const unsigned char y;
+};
+
+
+#endif //PP_SPACE_INVASION_BONUSENEMY_HPP
